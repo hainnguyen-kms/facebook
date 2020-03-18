2e00cc9a10ff3865f78b1a85978586427abed4e2
hainnguyen <hainnguyen@kms-technology.com>
2020-03-18 14:06:04 +0700

    Fix credential

--- src/main/java/com/example/reactive/config/DynamoConfig.java
+++ src/main/java/com/example/reactive/config/DynamoConfig.java
@@ -3,7 +3,9 @@ package com.example.reactive.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 import java.net.URI;
@@ -13,9 +15,8 @@ public class DynamoConfig {
     @Bean
     public DynamoDbAsyncClient dynamoDbAsyncClient(){
         return DynamoDbAsyncClient.builder()
-                .region(Region.AP_EAST_1)
-                .endpointOverride(URI.create("http://localhost:4569"))
-                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .region(Region.AP_SOUTHEAST_1)
+                .credentialsProvider(SystemPropertyCredentialsProvider.create())
                 .build();
     }
 
